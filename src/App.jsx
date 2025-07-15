8c95cd7235019892b2e746641c84e48d4ffa6410
Alex-mfoniso <alexandermfoniso18@gmail.com>
2025-07-15 13:57:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import AppRouter from "./routes/AppRouter";
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
     <>
       <Header />
       <AppRouter />
+      <Analytics/>
     </>
   );
 };
